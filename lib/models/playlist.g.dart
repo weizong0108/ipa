4c8d4abf7135ef31e168673b3038d8c9282c8ef3
weizong <your.email@example.com>
2025-04-30 11:11:44 +0800

    feat: 添加生成的 model 文件

---
+++ lib/models/playlist.g.dart
@@ -0,0 +1,62 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'playlist.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class PlaylistAdapter extends TypeAdapter<Playlist> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Playlist read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Playlist(
+      id: fields[0] as String,
+      name: fields[1] as String,
+      description: fields[2] as String,
+      coverUrl: fields[3] as String,
+      songs: (fields[4] as List).cast<Song>(),
+      createdBy: fields[5] as String,
+      createdAt: fields[6] as DateTime,
+      playCount: fields[7] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Playlist obj) {
+    writer
+      ..writeByte(8)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.description)
+      ..writeByte(3)
+      ..write(obj.coverUrl)
+      ..writeByte(4)
+      ..write(obj.songs)
+      ..writeByte(5)
+      ..write(obj.createdBy)
+      ..writeByte(6)
+      ..write(obj.createdAt)
+      ..writeByte(7)
+      ..write(obj.playCount);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PlaylistAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
